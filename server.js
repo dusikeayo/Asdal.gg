a616267ab2aa7067f0c561562b82daed9998442f
환동 <a01045878991@gmail.com>
2026-08-18 02:12:23 +0900

    Render 포트 설정 수정

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const path = require("path");
 const { URL } = require("url");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 
 // ==============================
